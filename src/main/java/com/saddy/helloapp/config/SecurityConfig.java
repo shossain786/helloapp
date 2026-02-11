93433a7d54d674b6045a667710a4518d30818fd9
Mohammad Saddam Hossain <sddmhossain786@gmail.com>
2026-02-11 10:45:36 +0530

    sprint security addeD

---
+++ src/main/java/com/saddy/helloapp/config/SecurityConfig.java
@@ -0,0 +1,43 @@
+package com.saddy.helloapp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails admin = User.withUsername("admin").password("admin").roles("ADMIN").build();
+
+        UserDetails user = User.withUsername("user").password("user").roles("USER").build();
+
+        return new InMemoryUserDetailsManager(admin, user);
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/h2-console/**").permitAll()
+                        .requestMatchers("/users/**").authenticated()
+                        .anyRequest().permitAll()
+                )
+                .httpBasic();
+
+        http.headers(headers -> headers.frameOptions().disable());
+
+        return http.build();
+    }
+}
